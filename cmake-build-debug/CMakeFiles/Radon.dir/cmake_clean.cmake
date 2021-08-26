4711dfe73fd931ba56d1fbec60c8bd93225fe6b3
OBueckert <owen.bueckert1@gmail.com>
2021-08-26 10:18:36 -0400

    added debug build directory

---
+++ cmake-build-debug/CMakeFiles/Radon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Radon.dir/src/App.cpp.o"
+  "CMakeFiles/Radon.dir/src/Main.cpp.o"
+  "Radon"
+  "Radon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Radon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
