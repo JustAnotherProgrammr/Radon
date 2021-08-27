958c4598ef9e0b456a3e1c22d628dc3a319d3cd5
OBueckert <owen.bueckert1@gmail.com>
2021-08-27 12:48:38 -0400

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import sys
+import subprocess
+import os
+
+if sys.platform.startswith("darwin"):
+	print("Detected OS: OSX\n")
+	subprocess.Popen("echo Radon Doesn't Support MacOS, Sorry :(", shell=True)
+elif sys.platform.startswith("win32"):
+	print("Detected OS: Windows\n")
+	os.system("mkdir dep")
+	os.chdir("dep")
+	os.system("mkdir wxWidgets-build")	
+	os.system("git clone --recurse-submodules https://github.com/wxWidgets/wxWidgets.git")
+	os.chdir("wxWidgets")
+	os.system("cmake . -DCMAKE_INSTALL_PREFIX=..\wxWidgets-build -DwxBUILD_SHARED=OFF -DwxUSE_PRIVATE_FONTS=ON")
+	os.system("cmake --build . --target install")
+	os.chdir("..\..")
+	os.system("cmake -DRA_DEBUG=OFF .")
+	os.system("cmake --build . --target Radon")
+	print("Compiled Radon for Windows")
+elif sys.platform.startswith("linux"):
+	print("Detected OS: Linux")
