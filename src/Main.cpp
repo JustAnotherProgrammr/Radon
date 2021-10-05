68bc225268ede6a7e816260d87485d7740b24818
Owen Bueckert <owen.bueckert1@gmail.com>
2021-10-05 18:16:19 -0400

    Add save/load system and window title update

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,11 +4,20 @@
 
 #include "Main.h"
 
+wxString defaultDocumentName = "Untitled Document";
+wxString exportPath;
+wxString importPath;
+
 wxBEGIN_EVENT_TABLE(Main, wxFrame)
+                EVT_MENU(10002, Main::OnMenuOpen)
+                EVT_MENU(10004, Main::OnMenuSave)
+                EVT_MENU(10005, Main::OnMenuSaveAs)
                 EVT_MENU(10006, Main::OnMenuExit)
+                EVT_MENU(10007, Main::OnEditMode)
+                EVT_MENU(10008, Main::OnViewMode)
 wxEND_EVENT_TABLE()
 
-Main::Main() : wxFrame(nullptr, wxID_ANY, "Radon - Untitled Document", wxPoint(0, 0), wxSize(800, 600))
+Main::Main() : wxFrame(nullptr, wxID_ANY, "Radon - " + defaultDocumentName, wxPoint(0, 0), wxSize(800, 600))
 {
     // Add private fonts
     wxFont InterEL = addPrivateFont("Inter-ExtraLight.ttf", "Inter-ExtraLight");
@@ -18,8 +27,7 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "Radon - Untitled Document", wxPoint(0
     // Add menu bar
     m_mbar = new wxMenuBar();
     this->SetMenuBar(m_mbar);
-
-    // Menu bar buttons
+    // Populate menu bar
     wxMenu *menuFile = new wxMenu();
     menuFile->Append(10001, "New");
     menuFile->Append(10002, "Open");
@@ -27,16 +35,26 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "Radon - Untitled Document", wxPoint(0
     menuFile->Append(10004, "Save");
     menuFile->Append(10005, "Save As");
     menuFile->Append(10006, "Exit");
-
     m_mbar->Append(menuFile, "File");
 
+    // Add toolbar
+    m_toolbar = this->CreateToolBar();
+    m_toolbar->AddTool(wxID_NEW, "New",
+                     wxNullBitmap, wxNullBitmap, wxITEM_DROPDOWN,
+                     "New file", "This is help for new file tool");
+    m_modemenu = new wxMenu;
+    m_modemenu->Append(10007, "&Edit Mode");
+    m_modemenu->Append(10008, "&View Mode");
+    m_toolbar->SetDropdownMenu(wxID_NEW, m_modemenu);
+    m_toolbar->Realize();
+
+
     // Add main text box, font and grid layout
-    m_tbx1 = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
-
-    m_tbx1->SetFont(InterReg);
-
+    m_tbx = new wxRichTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxVSCROLL | wxHSCROLL | wxRE_MULTILINE | wxBORDER_NONE | wxWANTS_CHARS);
+    m_tbx->SetFont(InterReg);
+    m_tbx->SetFilename(defaultDocumentName);
     wxGridSizer *m_gridSizer = new wxGridSizer(10, 10, 0, 0);
-    m_gridSizer->Add(m_tbx1, 1, wxEXPAND | wxALL);
+    m_gridSizer->Add(m_tbx, 1, wxEXPAND | wxALL);
 }
 
 wxFont Main::addPrivateFont(const wxString& fileName, const wxString& faceName)
@@ -72,23 +90,104 @@ void Main::OnMenuNew(wxCommandEvent & evt)
 
 void Main::OnMenuOpen(wxCommandEvent & evt)
 {
-
+    loadFile();
+    evt.Skip();
 }
 
 void Main::OnMenuSave(wxCommandEvent & evt)
 {
-
+    if (exportPath != wxEmptyString)
+    {
+        m_tbx->SaveFile(exportPath, wxRICHTEXT_TYPE_TEXT);
+    } else if(importPath != wxEmptyString)
+    {
+        m_tbx->SaveFile(importPath, wxRICHTEXT_TYPE_TEXT);
+#if RA_DEBUG
+        if (importPath.EndsWith(".txt"))
+        {
+            std::cout << "ReSaved " << importPath << std::endl;
+        } else
+        {
+            std::cout << "ReSaved " << importPath << ".txt" << std::endl;
+        }
+#endif
+    } else
+    {
+        Main::saveFile();
+    }
+    evt.Skip();
 }
 
 void Main::OnMenuSaveAs(wxCommandEvent & evt)
 {
-
+    saveFile();
+    evt.Skip();
 }
 
 void Main::OnMenuExit(wxCommandEvent & evt)
 {
     Close();
     evt.Skip();
+}
+
+void Main::OnEditMode(wxCommandEvent &evt)
+{
+    m_tbx->SetEditable(true);
+    m_tbx->GetCaret()->Show();
+    evt.Skip();
+}
+
+void Main::OnViewMode(wxCommandEvent &evt)
+{
+    m_tbx->SetEditable(false);
+    m_tbx->GetCaret()->Hide();
+    evt.Skip();
+}
+
+void Main::loadFile()
+{
+    wxString importName;
+    wxFileDialog fdlg(
+            this, ("Open File"), "", "", "Txt Files (*.txt)|*.txt", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
+    fdlg.ShowModal();
+    importPath = fdlg.GetPath();
+    importName = fdlg.GetFilename();
+    if (importPath != wxEmptyString) {
+        m_tbx->LoadFile(importPath, wxRICHTEXT_TYPE_TEXT);
+#if RA_DEBUG
+        std::cout << "Loaded " << importPath << std::endl;
+#endif
+    }
+    // Update the text box file name and wxFrame name
+    m_tbx->SetFilename(importName);
+    this->SetTitle("Radon - " + importName);
+}
+
+void Main::saveFile()
+{
+    wxString exportName;
+    wxFileDialog fdlg(
+            this, ("Save File"), "", "", "Txt Files (*.txt)|*.txt", wxFD_SAVE|wxFD_OVERWRITE_PROMPT);
+    fdlg.ShowModal();
+    if (fdlg.GetPath() != wxEmptyString)
+    {
+        if (fdlg.GetPath().EndsWith(".txt"))
+        {
+            exportPath = fdlg.GetPath();
+            exportName = fdlg.GetFilename();
+        } else
+        {
+            exportPath = fdlg.GetPath() << ".txt";
+            exportName = fdlg.GetFilename() << ".txt";
+        }
+        m_tbx->SaveFile(exportPath, wxRICHTEXT_TYPE_TEXT);
+#if RA_DEBUG
+        std::cout << "Saved " << exportPath << std::endl;
+#endif
+    }
+    // Update the text box file name and wxFrame name
+    m_tbx->SetFilename(exportName);
+    this->SetTitle("Radon - " + exportName);
 }
 
 Main::~Main()
--- src/Main.h
+++ src/Main.h
@@ -10,6 +10,8 @@
 #include <wx/font.h>
 #include <wx/stdpaths.h>
 #include <wx/filename.h>
+#include <wx/richtext/richtextctrl.h>
+#include <wx/filedlg.h>
 
 class Main : public wxFrame
 {
@@ -17,18 +19,23 @@ public:
     Main();
     ~Main();
 private:
-    wxFont addPrivateFont(const wxString& pathName, const wxString& faceName);
-
     void OnMenuNew(wxCommandEvent &evt);
     void OnMenuOpen(wxCommandEvent &evt);
     void OnMenuSave(wxCommandEvent &evt);
     void OnMenuSaveAs(wxCommandEvent &evt);
     void OnMenuExit(wxCommandEvent &evt);
+    void OnEditMode(wxCommandEvent &evt);
+    void OnViewMode(wxCommandEvent &evt);
 public:
-    wxTextCtrl *m_tbx1 = nullptr;
+    wxFont addPrivateFont(const wxString& pathName, const wxString& faceName);
+    void loadFile();
+    void saveFile();
+private:
+    wxRichTextCtrl *m_tbx = nullptr;
     wxFont *m_font1 = nullptr;
     wxMenuBar *m_mbar = nullptr;
-
+    wxToolBar *m_toolbar = nullptr;
+    wxMenu *m_modemenu = nullptr;
 wxDECLARE_EVENT_TABLE();
 };
 
